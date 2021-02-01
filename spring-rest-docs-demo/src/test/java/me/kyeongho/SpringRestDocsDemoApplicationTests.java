d3e3f6538959cf9033a836899c9625a83c9594cd
KyeonghoYoo <ykh6242@gmail.com>
2021-02-01 18:03:25 +0900

    프로젝트 생성

---
+++ spring-rest-docs-demo/src/test/java/me/kyeongho/SpringRestDocsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kyeongho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDocsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
